01cd7cbc378e58c498a3b60c3bbcdadc6d86bee0
Jawad Alamoushj <Jawadalamoush02@gmail.com>
2024-09-03 01:48:04 +0300

    explorer_button doesnt work right

--- app/src/main/java/com/example/eventure/AccountType.java
+++ app/src/main/java/com/example/eventure/AccountType.java
@@ -3,6 +3,7 @@ package com.example.eventure;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
@@ -33,5 +34,13 @@ public class AccountType extends AppCompatActivity {
             startActivity(intent);
         }
     });
+        Button explorer_button = findViewById(R.id.explorer_button);
+        explorer_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AccountType.this, Explorer_Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/eventure/Explorer_Login.java
@@ -0,0 +1,28 @@
+package com.example.eventure;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Explorer_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_explorer_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+//    TextView forgot_password = findViewById(R.id.explorer_login_forgot_password);
+
+}
