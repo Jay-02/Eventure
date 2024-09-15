7c466e5ea153bd5cc04048566786ce8548a5bed3
Jawad Alamoushj <Jawadalamoush02@gmail.com>
2024-09-15 19:26:41 +0300

    created SQLite Explorer Table and now I will hook it up to the model.

--- app/src/main/java/com/example/eventure/model/database/DatabaseHelper.java
+++ app/src/main/java/com/example/eventure/model/database/DatabaseHelper.java
@@ -1,5 +1,41 @@
 package com.example.eventure.model.database;
 
-public class DatabaseHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public final static String DATABASE_NAME = "EVENTURE_DATABASE.db";
+
+    public final static int DATABASE_VERSION= 1;
+
+//    explorers table
+    public final static String TABLE_EXPLORER = "explorer";
+    public final static String COLUMN_EXPLORER_USERNAME= "username";
+    public final static String COLUMN_EXPLORER_EMAIL = "email";
+    public final static String COLUMN_EXPLORER_PASSWORD = "password";
+    public final static String COLUMN_GENDER= "gender";
+    public final static String COLUMN_BIRTHDATE= "birthdate";
+
+    public static final String CREATE_EXPLORER_TABLE = "CREATE TABLE " + TABLE_EXPLORER + "("
+            + COLUMN_EXPLORER_USERNAME + "TEXT PRIMARY KEY, "
+            + COLUMN_EXPLORER_EMAIL + "TEXT, "
+            + COLUMN_EXPLORER_PASSWORD + "TEXT, "
+            + COLUMN_GENDER + "TEXT, "
+            + COLUMN_BIRTHDATE + "TEXT" + ")";
+    public DatabaseHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_EXPLORER_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + TABLE_EXPLORER);
+        onCreate(sqLiteDatabase);
+    }
 
 }
